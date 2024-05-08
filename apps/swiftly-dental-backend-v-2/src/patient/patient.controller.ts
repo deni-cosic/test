89fcf1a202f207831bf567002cec2428b253e9ea
Deni Cosic <deni.cosic@codeasy.com>
2024-05-08 14:33:05 +0200

    Add patient count endpoint

--- apps/swiftly-dental-backend-v-2/src/patient/patient.controller.ts
+++ apps/swiftly-dental-backend-v-2/src/patient/patient.controller.ts
@@ -3,6 +3,11 @@ import * as swagger from "@nestjs/swagger";
 import * as nestAccessControl from "nest-access-control";
 import { PatientService } from "./patient.service";
 import { PatientControllerBase } from "./base/patient.controller.base";
+import { ApiNestedQuery } from "src/decorators/api-nested-query.decorator";
+import { PatientFindManyArgs } from "./base/PatientFindManyArgs";
+import * as errors from "../errors";
+import { plainToClass } from "class-transformer";
+import { Request } from "express";
 
 @swagger.ApiTags("patients")
 @common.Controller("patients")
@@ -13,5 +18,18 @@ export class PatientController extends PatientControllerBase {
     protected readonly rolesBuilder: nestAccessControl.RolesBuilder
   ) {
     super(service, rolesBuilder);
+  }
+
+  @common.Get("count")
+  @swagger.ApiOkResponse({ type: [Number] })
+  @ApiNestedQuery(PatientFindManyArgs)
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async patientsCount(@common.Req() request: Request): Promise<number> {
+    const args = plainToClass(PatientFindManyArgs, request.query);
+    return this.service.count({
+      ...args,
+    });
   }
 }
