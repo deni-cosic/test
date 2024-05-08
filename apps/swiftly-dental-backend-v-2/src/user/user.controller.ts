dd9ad33c13a6e1c501f16a87938d260a983d8e74
Deni Cosic <deni.cosic@codeasy.com>
2024-05-08 11:46:13 +0200

    Add users/me route

--- apps/swiftly-dental-backend-v-2/src/user/user.controller.ts
+++ apps/swiftly-dental-backend-v-2/src/user/user.controller.ts
@@ -3,6 +3,13 @@ import * as swagger from "@nestjs/swagger";
 import * as nestAccessControl from "nest-access-control";
 import { UserService } from "./user.service";
 import { UserControllerBase } from "./base/user.controller.base";
+import { AclFilterResponseInterceptor } from "src/interceptors/aclFilterResponse.interceptor";
+import { User } from "./base/User";
+import * as errors from "../errors";
+import { UserWhereUniqueInput } from "./base/UserWhereUniqueInput";
+import { UserInfo } from "src/auth/UserInfo";
+import { JwtAuthGuard } from "src/auth/jwt/jwtAuth.guard";
+import { UserData } from "src/auth/userData.decorator";
 
 @swagger.ApiTags("users")
 @common.Controller("users")
@@ -13,5 +20,42 @@ export class UserController extends UserControllerBase {
     protected readonly rolesBuilder: nestAccessControl.RolesBuilder
   ) {
     super(service, rolesBuilder);
+  }
+
+  @common.UseGuards(JwtAuthGuard, nestAccessControl.ACGuard)
+  @common.UseInterceptors(AclFilterResponseInterceptor)
+  @common.Get("/me")
+  @swagger.ApiOkResponse({ type: User })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @nestAccessControl.UseRoles({
+    resource: "User",
+    action: "read",
+    possession: "own",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async user(@UserData() user: any): Promise<any | null> {
+    const result = await this.service.user({
+      where: { id: user.id },
+      select: {
+        createdAt: true,
+        email: true,
+        firstName: true,
+        id: true,
+        lastName: true,
+        roles: true,
+        updatedAt: true,
+        username: true,
+      },
+    });
+    if (result === null) {
+      throw new errors.NotFoundException(
+        `No resource was found for logged in user`
+      );
+    }
+
+    (result as any).role = (result.roles as any)[0];
+    return result;
   }
 }
