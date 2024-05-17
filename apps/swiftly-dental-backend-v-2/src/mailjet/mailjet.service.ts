0fd61800ebcba6a94ea6057d06c35d316e1a0567
Deni Cosic <deni.cosic@codeasy.com>
2024-05-17 09:30:52 +0200

    Add logs for key debug

--- apps/swiftly-dental-backend-v-2/src/mailjet/mailjet.service.ts
+++ apps/swiftly-dental-backend-v-2/src/mailjet/mailjet.service.ts
@@ -1,22 +1,28 @@
-import { Inject, Injectable } from '@nestjs/common'
-import { Client, SendEmailV3_1, LibraryResponse } from 'node-mailjet'
-import { MAILJET_CONFIG } from './mailjet.constants'
+import { Inject, Injectable } from "@nestjs/common";
+import { Client, SendEmailV3_1, LibraryResponse } from "node-mailjet";
+import { MAILJET_CONFIG } from "./mailjet.constants";
 
-import { MailjetConfig } from './MailjetConfig'
+import { MailjetConfig } from "./MailjetConfig";
 
 @Injectable()
 export class MailjetService {
-  mailJet: Client
+  mailJet: Client;
 
-  constructor(@Inject(MAILJET_CONFIG) private readonly mailjetConfig: MailjetConfig) {
-    const { publicKey, privateKey } = this.mailjetConfig
+  constructor(
+    @Inject(MAILJET_CONFIG) private readonly mailjetConfig: MailjetConfig
+  ) {
+    const { publicKey, privateKey } = this.mailjetConfig;
+
+    console.log({ apiKey: publicKey, apiSecret: privateKey });
     this.mailJet = new Client({
       apiKey: publicKey,
       apiSecret: privateKey,
-    })
+    });
   }
 
-  sendEmail(data: SendEmailV3_1.Body): Promise<LibraryResponse<SendEmailV3_1.Response>> {
-    return this.mailJet.post('send', { version: 'v3.1' }).request(data)
+  sendEmail(
+    data: SendEmailV3_1.Body
+  ): Promise<LibraryResponse<SendEmailV3_1.Response>> {
+    return this.mailJet.post("send", { version: "v3.1" }).request(data);
   }
 }
